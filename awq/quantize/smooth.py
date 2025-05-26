19a0f21b9a360e62ab4a5050eaafe4f4fd3af0a8
Haisheng Chen <langzs335@outlook.com>
2025-05-26 15:46:11 -0700

    remove llava requirements

--- awq/quantize/smooth.py
+++ awq/quantize/smooth.py
@@ -2,11 +2,11 @@
 
 
 import torch.nn as nn
-import llava
-from llava.media import Image, Video
-from llava.utils.media import extract_media
-from llava.constants import DEFAULT_IMAGE_TOKEN
-from llava.mm_utils import process_image, process_images
+#import llava
+#from llava.media import Image, Video
+#from llava.utils.media import extract_media
+#from llava.constants import DEFAULT_IMAGE_TOKEN
+#from llava.mm_utils import process_image, process_images
 import torch
 from collections import defaultdict
 from functools import partial
